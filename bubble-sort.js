d2cf8b888423a16ca35dee547d93ef88bdbe69b6
demirose41 <demirose41@gmail.com>
2024-10-19 17:16:36 -0700

    done

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,13 +1,23 @@
 
 function bubbleSort(arr) {
-
-    // Iterate through the array
-
-      // If the current value is greater than its neighbor to the right
-        // Swap those values
-
-        // Do not move this console.log
+  // Iterate through the array
+  let sorted = false;
+  while(!sorted){      
+    sorted = true;
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] > arr[i+1]){
+        [arr[i],arr[i+1]] = [arr[i+1], arr[i]];
         console.log(arr.join(","));
+        sorted = false;
+      }
+    }
+    }
+    // If the current value is greater than its neighbor to the right
+    // Swap those values
+    
+    // Do not move this console.log
+            
+    return 
 
     // If you get to the end of the array and no swaps have occurred, return
 
